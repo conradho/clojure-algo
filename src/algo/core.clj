832deb41218ad6674a56d47f70b908bbfcc0a759
conrad <conrad@pythonanywhere.com>
2016-01-16 13:30:08 +0000

    rewrite merge-and-count to optimize
    use tail recursion to avoid stack overflow by using tail recursion, and
    keep track of sequence length to avoid counting each time -> which would
    have been O(n^n) runtime.

--- src/algo/core.clj
+++ src/algo/core.clj
@@ -28,18 +28,22 @@
           (merge-sort-algo left)
           (merge-sort-algo right))))))
 
-(defn merge-and-count [left right accum-count]
-  ;; merged func name definition row with argument vector
-  ;; to the same line because no docstring in between
-    (cond (nil? left) [right accum-count]
-          (nil? right) [left accum-count]
-          :else (let [[l & *left] left
-                      [r & *right] right]
-                  (if (< l r)
-                    (let [[merged final-count] (merge-and-count *left right accum-count)]
-                      [(cons l merged) final-count])
-                    (let [[merged final-count] (merge-and-count left *right (inc (+ accum-count (count *left))))]
-                      [(cons r merged) final-count])))))
+(defn merge-and-count
+  "think of the world as three jars of pebbles and a score board
+  this function moves around the pebbles and increments the score board
+  but leaves everything else the same/no side effects"
+  [left right]
+  (loop [left left, right right, left-len (count left),
+         merged [], accum-count 0]  ; initialize merged & accum-count
+    (if (or (nil? left) (nil? right))
+      ;; at the end, return just the merged list and the count params
+      [(concat merged left right) accum-count]
+      ;; do tail recursion with (left, right, merged, count) params
+      (let [[l-head & l-tail] left
+            [r-head & r-tail] right]
+        (if (< l-head r-head)
+          (recur l-tail right (dec left-len) (conj merged l-head)  accum-count)
+          (recur left r-tail left-len (conj merged r-head) (+ accum-count left-len)))))))
 
 (defn count-inversion
   "piggy backing on merge sort to count the number of inversions"
@@ -50,8 +54,10 @@
         [xs accum-inv-count]
         (let [[left right] (split-at (/ xs-len 2) xs)
               [sorted-left count-left] (count-inversion left 0)
-              [sorted-right count-right] (count-inversion right 0)]
-          (merge-and-count sorted-left sorted-right (+ accum-inv-count count-left count-right)))))))
+              [sorted-right count-right] (count-inversion right 0)
+              [merged merge-count] (merge-and-count sorted-left sorted-right)]
+          [merged (+ merge-count count-left count-right)]
+          )))))
 
 
 (def xs (map read-string (clojure.string/split (slurp "IntegerArray.txt") #"\r\n")))
