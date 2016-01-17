cb14e84a1605f332e208adebf0aa926010e2e90c
conrad <conrad@pythonanywhere.com>
2016-01-17 10:03:13 +0000

    optimization parameters

--- project.clj
+++ project.clj
@@ -4,4 +4,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  ;; :aot [algo.core]
+  :jvm-opts ["-Xms16G" "-Xmx16G" "-Xss16G"
+             ;; performance options
+             ;; http://www.oracle.com/technetwork/java/javase/tech/vmoptions-jsp-140102.html#PerformanceTuning
+             "-XX:+AggressiveOpts"
+             "-XX:+OptimizeStringConcat"
+             ;; garbage collection method
+             ;; http://docs.oracle.com/javase/8/docs/technotes/guides/vm/gctuning/g1_gc_tuning.html#g1_gc_tuning
+             ;; "-XX:+UseG1GC"
+             "-XX:+UseConcMarkSweepGC"
+             ;; debugging stuff
+             "-verbose:gc"
+             ;; "-XX:+PrintGCDetails"
+             "-XX:+PrintGCDateStamps"
+             ]
   :main algo.core)
